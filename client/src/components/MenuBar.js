43cf0bed57150d08d38a06118e42ec22a7375199
Rodrigo Durante <rodrigo.durantee@hotmail.com>
2020-03-24 17:20:37 -0300

    .

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 function MenuBar() {
   const pathname = window.location.pathname;
-
+  //
   const path = pathname === "/" ? "home" : pathname.substr(1);
   const [activeItem, setActiveItem] = useState(path);
 
